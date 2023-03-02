ea258b4d77cb8ee6e467014e307c0e64357c0b02
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2023-03-02 12:24:16 -0800

    Update config.php

--- register_login/config.php
+++ register_login/config.php
@@ -1,10 +1,10 @@
 <?php
 // Fresno State PostgreSQL Database credentials
-define('HOST', 'stem-scale-db.priv.fresnostate.edu');
-define('PORT', '5432');
-define('DB', 'swa');
-define('USER', 'scale_dyna');
-define('PASS', 'ZAKh55Mxxafz7jBqwhy_SG23C8_WkXm8_6');
+define('HOST', '');
+define('PORT', '');
+define('DB', '');
+define('USER', '');
+define('PASS', '');
 
 // Attempt to connect to the PostgreSQL database 
 $con = pg_connect("host=" . HOST . " port=" . PORT . " dbname=" . DB . " user=" . USER . " password=" . PASS)
